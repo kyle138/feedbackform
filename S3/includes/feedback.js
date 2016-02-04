e0fe52d27bf4f5b1b68249925863ba30514a5894
Kyle Munz on Snoopy <kyle138@gmail.com>
2016-02-03 18:36:00 -0600

    Frontend files in S3 folder added. Readme file added.

---
+++ S3/includes/feedback.js
@@ -0,0 +1,75 @@
+/*
+  Jquery Validation using jqBootstrapValidation
+  */
+$(function() {
+
+ $("input,textarea").jqBootstrapValidation(
+    {
+     preventSubmit: true,
+     submitError: function($form, event, errors) {
+      // something to have when submit produces an error ?
+      // Not decided if I need it yet
+     },
+     submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // Get values from FORM
+      // If any additional fields are added they need to be captured here.
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var subject = $("input#subject").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+	    firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+	// Convert form data variables to JSON array for AWS API Gateway
+	var formdata = {name: name,email: email,subject: subject,message: message};
+	formdata = JSON.stringify(formdata);
+   $.ajax({
+    url: "https://tn7x5hgwhb.execute-api.us-east-1.amazonaws.com/prod/feedback_hen/",
+		dataType: 'json',
+    type: "POST",
+		data: formdata,
+    cache: false,
+            	success: function() {
+            	// Success message
+            	   $('#success').html("<div class='alert alert-success'>");
+            	   $('#success > .alert-success').html("")
+            		.append( "</button>");
+            	  $('#success > .alert-success')
+            		.append("Thank you, your feedback has been received.");
+ 		  $('#success > .alert-success')
+ 			.append('</div>');
+
+ 		  //clear all fields
+ 		  $('#contactForm').trigger("reset");
+ 	      },
+ 	   error: function() {
+ 		// Fail message
+ 		 $('#success').html("<div class='alert alert-danger'>");
+            	$('#success > .alert-danger').html("")
+            	 .append( "</button>");
+            	$('#success > .alert-danger').append("WHOA! Sorry "+firstName+", it seems my email system is having a moment... Please email me directly to <a href='mailto:hen_feedback@hartenergy.com'>hen_feedback@hartenergy.com</a>.");
+ 	        $('#success > .alert-danger').append('</div>');
+ 		//clear all fields
+ 		$('#contactForm').trigger("reset");
+ 	    },
+           })
+         },
+         filter: function() {
+                   return $(this).is(":visible");
+         },
+       });
+
+      $("a[data-toggle=\"tab\"]").click(function(e) {
+                    e.preventDefault();
+                    $(this).tab("show");
+        });
+  });
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+     $('#success').html('');
+  });
---
+++ readme.txt
@@ -0,0 +1,17 @@
+Serverless feedback form using Bootstrap, JQuery, S3, Lambda, DynamoDB, and API Gateway.
+The walkthroughs below provide the front end code for a serverless contact or feedback form
+as well as the necessary steps in AWS to setup an API Gateway, IAM roles, Lambda function, and a DynamoDB Table.
+
+The code in this repo is split into two folders, S3 and lambda.
+All files and folders within the S3 folder will need to go within an S3 bucket configured for web hosting.
+This serves the front end HTML as well as the clientside javascript files required for collecting the form
+information and submitting the data to the API Gateway.
+The lambda folder contains a single lambdafunc.js file. The contents of this file will be copied into
+your lambda function from within the AWS console.
+
+Credits given below::
+Bootstrap Jquery form source:
+http://myprogrammingblog.com/2013/08/27/how-to-make-a-contact-form-with-bootstrap-3-jqueryphphtml5jqbootstrapvalidation/
+
+Lambda/DynamoDB/API Gateway walk through:
+http://docs.aws.amazon.com/apigateway/latest/developerguide/getting-started.html
