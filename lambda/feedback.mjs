a2460231a8efe6f40a27dfa834c214ec61839236
Kyle Munz <kyle138@gmail.com>
2026-05-24 14:39:28 -0500

    Feedback checks for message and score

--- lambda/feedback.mjs
+++ lambda/feedback.mjs
@@ -11,7 +11,73 @@
  * 
  */
 
+// validateRequiredVar
+// Checks if the supplied variable is of type string and has length
+// @param {var} reqvar - the variable to check
+// @return {promise} - Error or response object
+function validateRequiredVar(reqvar) {
+  return new Promise((resolve,reject) => {
+    // Is the envar a string and have some length?
+    console.log(`validateRequiredVar:reqvar:: ${reqvar}`);  // DEBUG
+    if(typeof reqvar === 'string' && reqvar.length > 0) {
+      return resolve(true);
+    } else {
+      return reject(new Error('Missing Required Variable'));
+    }
+  }); // End Promise
+} // End validateRequiredvar
+
+// robotrap
+// Checks for sufficient empathetic response (hint: we want it to be empty)
+// @param {var} vrbl - the variable to check
+// @return {promise} - Error or response object
+function robotrap(vrbl) {
+  return new Promise(async (resolve,reject) => {
+    // Does the vrbl have a value?
+    console.debug(`robotrap:vrbl:: ${vrbl}`); // DEBUG
+
+    if (vrbl === undefined ) {
+      console.debug(`robotrap: vrbl is undefined.`); // DEBUG
+      return resolve();
+    } else {
+      // vrbl isn't undefined but it might still be something...
+      await validateRequiredVar(vrbl)
+      .then(() => {
+        console.log(`Game: Checkers, Number of Players: 0`); // Bad robot!
+        return reject('Insuffient empathetic response');
+      })
+      .catch((err) => {
+        // In this case, error is good.
+        console.debug(`No trap.`); // DEBUG
+        return resolve();
+      })
+    }
+  }); // End Promise
+} // End robotrap
+
+// ************
+// Main handler
 export const handler = async (event, context) => {
+  console.log(`Received event: ${JSON.stringify(event,null,2)}`); // DEBUG:
+
+  var DateTime = new Date().toString();
+
+  var eventObj = JSON.parse(event?.body);
+  console.debug(`eventObj: `,JSON.stringify(eventObj,null,2)); // DEBUG Yeah I parsed it to stringify it
+  console.debug(`eventObj.message: ${eventObj.message}`); // DEBUG
+
+  // ******************
+  // Fields should be::
+  // name, email, site, subject, message, score (trap)
+
+
+  // Message is a required field, but Score is *not*.
+  return await Promise.all([
+    validateRequiredVar(eventObj?.message),
+    robotrap(eventObj?.score)
+  ]) 
+  .then((resp) => {
+    console.debug(`Promise.all.then.resp...`,resp); // DEBUG
     const response = {
       statusCode: 200,
       body: JSON.stringify({
@@ -20,5 +86,11 @@ export const handler = async (event, context) => {
     };
 
     return response;
-  };
+    
+  })
+  .catch((err) => {
+    console.debug(`Error:..`,err); // DEBUG
+  }); // End ValidateRequiredVar
+
+};  // End Handler
   
